4c435ed167adda2fb231074900cca0d97cfb6f71
fenikhey88 <limonprog@gmail.com>
2025-09-11 01:43:17 +0300

    working_server

--- giftkick_server/server.js
+++ giftkick_server/server.js
@@ -1,9 +1,7 @@
-// server.js
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
